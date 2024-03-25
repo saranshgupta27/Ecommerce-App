6da7e8ffb60c72d7a2453a30eab8bb9f6e450eb5
Saransh Gupta <saransh.gupta27@gmail.com>
2024-03-25 09:25:33 +0530

    feat: improve routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { useEffect } from "react";
 import { routes } from "~/constants/routes";
+import { useAuth } from "~/utils/AuthContext";
 
 export default function Home() {
+  const { user } = useAuth();
   useEffect(() => {
+    if (user) {
+      return (window.location.href = routes.INTERESTS);
+    }
     window.location.href = routes.LOGIN;
   });
 
