e467565f12c0334530cfda5add3574ee2c37fa6a
Saransh Gupta <saransh.gupta27@gmail.com>
2024-03-25 14:54:39 +0530

    fix: cjs attempt

--- src/pages/interests/index.tsx
+++ src/pages/interests/index.tsx
@@ -1,9 +1,7 @@
-import { useRouter } from "next/navigation";
 import InterestsForm from "~/components/forms/InterestsForm";
 import { useAuth } from "~/utils/AuthContext";
 
 function Interests() {
-  const router = useRouter();
   const { user } = useAuth();
 
   if (user && typeof window !== "undefined") {
